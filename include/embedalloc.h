9c51a49c22e4172c74bc921396a2c69fda855976
aceinetx <mouselin82@gmail.com>
2025-09-28 19:31:18 +0300

    format code

--- include/embedalloc.h
+++ include/embedalloc.h
@@ -4,24 +4,24 @@
 typedef unsigned long long eaull;
 
 typedef struct block {
-  eaull size;
-  char in_use;
-  struct block *next;
-  struct block *prev;
+    eaull size;
+    char in_use;
+    struct block* next;
+    struct block* prev;
 } block_t;
 
 typedef struct Allocator {
-  block_t *head;
-  void *start;
-  void *end;
+    block_t* head;
+    void* start;
+    void* end;
 } Allocator;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-Allocator ealloc_new(void *start, eaull size);
-void *ealloc(Allocator *ator, eaull size);
-void efree(Allocator *ator, void *ptr);
+Allocator ealloc_new(void* start, eaull size);
+void* ealloc(Allocator* ator, eaull size);
+void efree(Allocator* ator, void* ptr);
 #ifdef __cplusplus
 }
 #endif
--- src/embedalloc.c
+++ src/embedalloc.c
@@ -4,92 +4,92 @@
 #define false ((char)0)
 
 Allocator ealloc_new(void* start, eaull size) {
-  Allocator inst;
-  inst.start = start;
-  inst.end = (char*)start + size;
+    Allocator inst;
+    inst.start = start;
+    inst.end = (char*)start + size;
 
-  block_t* block = (block_t*)start;
-  block->size = size;
+    block_t* block = (block_t*)start;
+    block->size = size;
 
-  inst.head = (block_t*)block;
-  inst.head->next = NULL;
-  inst.head->prev = NULL;
-  return inst;
+    inst.head = (block_t*)block;
+    inst.head->next = NULL;
+    inst.head->prev = NULL;
+    return inst;
 }
 
 void* ealloc(Allocator* ator, eaull size) {
-  // align size to 8 bytes
-  size = (size + 7) & ~7;
+    // align size to 8 bytes
+    size = (size + 7) & ~7;
 
-  eaull total_needed = sizeof(block_t) + size;
+    eaull total_needed = sizeof(block_t) + size;
 
-  // find a block by first-fit
-  block_t* block = ator->head;
-  for (;;) {
-    if (!block)
-      break;
-    if (block->size >= total_needed && !block->in_use) {
-      // found a block
-      break;
+    // find a block by first-fit
+    block_t* block = ator->head;
+    for (;;) {
+        if (!block)
+            break;
+        if (block->size >= total_needed && !block->in_use) {
+            // found a block
+            break;
+        }
+        block = block->next;
     }
-    block = block->next;
-  }
 
-  if (!block)
-    return NULL;
+    if (!block)
+        return NULL;
 
-  if (block->size > total_needed) {
-    // split the block
-    block_t* next = (block_t*)((char*)block + total_needed);
-    next->size = block->size - total_needed;
-    next->prev = block;
-    next->next = NULL;
-    next->in_use = false;
+    if (block->size > total_needed) {
+        // split the block
+        block_t* next = (block_t*)((char*)block + total_needed);
+        next->size = block->size - total_needed;
+        next->prev = block;
+        next->next = NULL;
+        next->in_use = false;
 
-    block->size = total_needed;
-    block->next = next;
-  }
-  block->in_use = true;
+        block->size = total_needed;
+        block->next = next;
+    }
+    block->in_use = true;
 
-  // ator->head = block;
+    // ator->head = block;
 
-  void* ptr = block;
-  ptr += sizeof(block_t);
+    void* ptr = block;
+    ptr += sizeof(block_t);
 
-  return ptr;
+    return ptr;
 }
 
 void efree(Allocator* ator, void* ptr) {
-  if (!ptr)
-    return;
+    if (!ptr)
+        return;
 
-  block_t* block = (block_t*)(ptr - sizeof(block_t));
-  block->in_use = false;
+    block_t* block = (block_t*)(ptr - sizeof(block_t));
+    block->in_use = false;
 
-  // coalesce with next free blocks
-  {
-    block_t* next = block->next;
-    while (next && !next->in_use) {
-      block->size += next->size;
-      block->next = next->next;
-      if (next->next) {
-        next->next->prev = block;
-      }
-      next = block->next;
+    // coalesce with next free blocks
+    {
+        block_t* next = block->next;
+        while (next && !next->in_use) {
+            block->size += next->size;
+            block->next = next->next;
+            if (next->next) {
+                next->next->prev = block;
+            }
+            next = block->next;
+        }
     }
-  }
 
-  // coalesce with previous free blocks
-  {
-    block_t* prev = block->prev;
-    while (prev && !prev->in_use) {
-      prev->size += block->size;
-      prev->next = block->next;
-      if (block->next) {
-        block->next->prev = prev;
-      }
-      block = prev;
-      prev = block->prev;
+    // coalesce with previous free blocks
+    {
+        block_t* prev = block->prev;
+        while (prev && !prev->in_use) {
+            prev->size += block->size;
+            prev->next = block->next;
+            if (block->next) {
+                block->next->prev = prev;
+            }
+            block = prev;
+            prev = block->prev;
+        }
     }
-  }
 }
